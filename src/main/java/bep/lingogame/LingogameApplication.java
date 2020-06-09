58c4cd1a4d35e9bb339006f5f8f2c25cbe206686
hamza_el9@live.nl <Kam12340>
2020-06-09 12:29:02 +0200

    first push

---
+++ src/main/java/bep/lingogame/LingogameApplication.java
@@ -0,0 +1,13 @@
+package bep.lingogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingogameApplication.class, args);
+	}
+
+}
